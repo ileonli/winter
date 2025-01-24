42aeda719053d08ef3b8bbe9fefb04f1a65a94ec
Leon Li <meetleonli@gmail.com>
2025-01-24 17:46:45 +0800

    add: applyPropertyValues

--- src/main/java/io/github/ileonli/winter/factory/support/AbstractAutowireCapableBeanFactory.java
+++ src/main/java/io/github/ileonli/winter/factory/support/AbstractAutowireCapableBeanFactory.java
@@ -1,7 +1,11 @@
 package io.github.ileonli.winter.factory.support;
 
-import io.github.ileonli.winter.BeansException;
+import io.github.ileonli.winter.*;
 import io.github.ileonli.winter.factory.config.BeanDefinition;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFactory {
 
@@ -18,8 +22,56 @@ public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFac
 
     protected Object doCreateBean(String beanName, BeanDefinition beanDefinition) throws BeansException {
         Object instantiate = instantiationStrategy.instantiate(beanDefinition);
+        applyPropertyValues(beanName, instantiate, beanDefinition);
         addSingleton(beanName, instantiate);
         return instantiate;
+    }
+
+    protected void applyPropertyValues(String beanName, Object existBean, BeanDefinition beanDefinition) {
+        if (existBean == null || beanDefinition == null) {
+            throw new IllegalArgumentException("Bean instance or BeanDefinition must not be null");
+        }
+
+        PropertyValues pvs = beanDefinition.getPropertyValues();
+        if (pvs == null || pvs.isEmpty()) {
+            return;
+        }
+
+        Class<?> beanClass = beanDefinition.getBeanClass();
+        for (PropertyValue pv : pvs.getPropertyValues()) {
+            try {
+                applyPropertyValue(existBean, beanClass, pv);
+            } catch (BeansException e) {
+                throw new BeansException("Failed to apply property '" + pv.getName() + "' to bean '" + beanName + "'", e);
+            }
+        }
+    }
+
+    private void applyPropertyValue(Object bean, Class<?> beanClass, PropertyValue pv) {
+        String fieldName = pv.getName();
+        Object value = pv.getValue();
+
+        if (value == null) {
+            throw new BeansException("Property value for field '" + fieldName + "' is null");
+        }
+
+        try {
+            Field field = beanClass.getDeclaredField(fieldName);
+            Class<?> fieldType = field.getType();
+
+            String setMethodName = BeanUtils.fieldNameToSetMethodName(fieldName);
+            Method setMethod = beanClass.getDeclaredMethod(setMethodName, fieldType);
+
+            ReflectionUtils.makeAccessible(setMethod);
+
+            setMethod.invoke(bean, value);
+        } catch (NoSuchFieldException e) {
+            throw new BeansException("No such field '" + fieldName + "' in class " + beanClass.getName(), e);
+        } catch (NoSuchMethodException e) {
+            throw new BeansException("No such setter method for field '" + fieldName + "' in class " + beanClass.getName(), e);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            throw new BeansException("Failed to set value for field '" + fieldName + "' in class " + beanClass.getName(), e);
+        }
     }
 
 }
--- src/test/java/io/github/ileonli/winter/factory/support/DefaultListableBeanFactoryTest.java
+++ src/test/java/io/github/ileonli/winter/factory/support/DefaultListableBeanFactoryTest.java
@@ -1,17 +1,62 @@
 package io.github.ileonli.winter.factory.support;
 
+import io.github.ileonli.winter.PropertyValue;
+import io.github.ileonli.winter.PropertyValues;
 import io.github.ileonli.winter.factory.config.BeanDefinition;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 
 public class DefaultListableBeanFactoryTest {
 
     public static class TestClass {
+
+        private int a;
+        private String b;
+
+        public TestClass() {
+        }
+
+        public TestClass(int a, String b) {
+            this.a = a;
+            this.b = b;
+        }
+
+        public int getA() {
+            return a;
+        }
+
+        public void setA(int a) {
+            this.a = a;
+        }
+
+        public String getB() {
+            return b;
+        }
+
+        public void setB(String b) {
+            this.b = b;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (o == null || getClass() != o.getClass()) return false;
+            TestClass testClass = (TestClass) o;
+            return a == testClass.a && Objects.equals(b, testClass.b);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(a, b);
+        }
+
     }
 
     @Test
-    public void test() {
+    public void singletonObjectTest() {
         DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
         beanFactory.registerBeanDefinition("testClass", new BeanDefinition(TestClass.class));
 
@@ -21,4 +66,21 @@ public class DefaultListableBeanFactoryTest {
         Object testClass2 = beanFactory.getBean("testClass");
         assertEquals(testClass1, testClass2);
     }
+
+    @Test
+    public void applyPropertyValuesTest() {
+        PropertyValues pvs = new PropertyValues();
+        pvs.addPropertyValue(new PropertyValue("a", 10));
+        pvs.addPropertyValue(new PropertyValue("b", "test"));
+
+        BeanDefinition bd = new BeanDefinition(TestClass.class, pvs);
+
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+        beanFactory.registerBeanDefinition("testClass", bd);
+
+        TestClass testClass = (TestClass) beanFactory.getBean("testClass");
+        assertEquals(10, testClass.getA());
+        assertEquals("test", testClass.getB());
+    }
+
 }
